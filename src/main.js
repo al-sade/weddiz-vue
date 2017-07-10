5861b552001809c5dad3ab70e388d0bb2195127f
al-sade <alsade15@gmail.com>
2017-07-11 01:00:42 +0300

    vue-resource

--- src/main.js
+++ src/main.js
@@ -5,21 +5,25 @@ import Vue from 'vue'
 import App from './App'
 
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 
 import Navigation from './components/partials/Navigation'
 import Home from '@/components/Home'
+import Suppliers from '@/components/Suppliers'
 import Contact from '@/components/Contact'
 
 Vue.use(ElementUI)
+Vue.use(VueResource)
 Vue.use(Router)
 
 Vue.config.productionTip = false
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/test', component: Contact }
+    { path: '/suppliers', component: Suppliers },
+    { path: '/contact', component: Contact }
 ]
 
 const router = new Router({routes})

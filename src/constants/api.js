04ee0f93a71fcfcc6c4036e8b3c099f920115cb5
al-sade <alsade15@gmail.com>
2017-10-01 23:49:06 +0300

    pre-stage #4

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,12 +1,10 @@
-import config from "./config";
-const host = config.api.host;
+import config from './config'
+const host = config.api.host
 const API = {
-    categories: host + "categories",
-    suppliersByCategory: (categoryId) => {return host + "suppliers/" + categoryId},
-    albums: host + "albums",
-    supplierAlbums: (supplierId) => {return host + "albums/" + supplierId }
-    
-    
-};
+  categories: host + 'categories',
+  suppliersByCategory: (categoryId) => { return host + 'suppliers/' + categoryId },
+  albums: host + 'albums',
+  supplierAlbums: (supplierId) => { return host + 'albums/' + supplierId }
+}
 
-export default API;
+export default API
